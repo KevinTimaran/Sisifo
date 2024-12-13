fae14a4505fb750f2379cfa5c5ff4e652daf2b07
Kevin Stiven Timaran <kevinstiventimaran@gmail.com>
2024-12-12 19:36:08 -0500

    CodigoParaUnaVentana

---
+++ 1/SegundaPractica.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+
+# Crear una ventana principal
+ventana = tk.Tk()  # Asegúrate de usar mayúscula 'T' en 'Tk'
+ventana.title("Este es un titulo")
+ventana.geometry("300x300")
+
+# Crear una etiqueta
+etiqueta = tk.Label(ventana, text="Esto es una etiqueta")
+etiqueta.pack(pady=100)
+
+# Definir la función que se ejecutará al hacer clic en el botón
+def click():
+    print("Me has hecho click")
+
+# Crear un botón y asignar la función 'click' al comando
+boton = tk.Button(ventana, text="Hazme click", command=click)
+boton.pack()
+
+# Iniciar el bucle principal de la ventana
+ventana.mainloop()
