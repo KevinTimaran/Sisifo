324e18d83e0ac3816b7ea0ddee921d013e54400c
kevin Stiven Timaran <kevinstiventimaran@gmail.com>
2024-12-13 22:34:18 -0500

    Metodos de cadenas

---
+++ 1/Metodos_de_cadenas.py
@@ -0,0 +1,19 @@
+# (dir) devuelve la lista de atrubutos validos del objeto 
+# (uper) Combierte a mayusculas
+# (lower) combierte a mayusculas 
+# (capitalize) Primera en mayuscula 
+
+# (find) Metodo encuentra la primera aparicion del valor espacificado, sino devuelve 1
+# (index) Metodo encuentra la primera aparicion del valor especificado, sino devuelve una excepcion
+
+# (isnumeric) si es numerico devuelve true
+# ( isalpha) si el alfa numerico devuelve true
+
+# (count) Devuelve el numero de ocurrencias de una sebcadena en la dada
+# (len) cuenta los caracteres de una cadena
+# 
+# (endswith) verifica si es una cadena comienza con
+# (starswith)virifica si una cadena termina con 
+
+#(replace) remplaza un valor por otro 
+#(split) separa por el parametro dado
