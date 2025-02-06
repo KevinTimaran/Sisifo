0e63bffa22fb8958850e32f1e7ebcb6942476dca
Kevin Besaquillo <kevinstiventimaran@gmail.com>
2025-02-06 09:55:23 -0500

    Calculadora primer modelo

--- Practicas/Calculadora/problema.py
+++ Practicas/Calculadora/problema.py
@@ -1,19 +1,17 @@
 
-class problema:
-    def __init__(self,datoA,Operacion, datoB):
-        self.datoA = datoA
-        self.datoB = datoB
-        self.operacion = Operacion
+class Datos:
+    def __init__(self, dato1, signo, dato2):
+        self.dato1 = dato1
+        self.signo = signo
+        self.dato2 = dato2
 
-    #----------------------
-    #Metodos
-    #----------------------
-
-    def darDatoA (self):
-        return self.datoA
+    def darDato1 (self):
+        return self.dato1
     
-    def darDatoB (self,):
-        return self.datoB
+    def darOperacion (self):
+        return self.signo
     
-    def darOperacion (self,):
-        return self.operacion
+    def darDato2 (self):
+        return self.dato2
+        
+    
