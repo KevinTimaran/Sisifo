6149a0c9bb51f801614f4379c8c4d8a6e7c1d0e2
KevinTimaran <kevin.buesaquillo@campusucc.edu.co>
2025-02-27 19:58:30 -0500

    Semana 4

--- Proyectos Uni/Segundo Semestre/Momento1/Mercado/ProductosMercado.py
+++ Proyectos Uni/Segundo Semestre/Momento1/Mercado/ProductosMercado.py
@@ -1,13 +1,18 @@
 ListaProductos = []
 
 class Productos: 
-    def __init__(self, Producto, Precio):
+    def __init__(self, Producto, Precio, Cantidad):
         self.Producto = Producto
         self.Precio = Precio
-
+        self.Cantidad = Cantidad
         Diccionario={
             'Producto':Producto,
-            'Precio': Precio
+            'Precio': Precio,
+            'Cantidad': Cantidad
         }
         ListaProductos.append(Diccionario)
     
+    def ListaProductos ():
+
+        for i, Diccionario in enumerate(ListaProductos):
+            print (f"""{i}. {Diccionario['Producto']} con un valor de {Diccionario['Precio']}""")
--- Proyectos Uni/Segundo Semestre/Momento1/Mercado/main.py
+++ Proyectos Uni/Segundo Semestre/Momento1/Mercado/main.py
@@ -1,30 +1,57 @@
+from ProductosMercado import Productos
 
-def darOpcion():
-    if Opciones == "1":
-        return agregarProductos()
-    if Opciones =="2":
-        return agregarACarrito()
+def darInicio ():
+    a = input ("""1 si desea ir al menu
+    2 si desea terminar 
+    Digite un numero: """)
+
+    if a == "1":
+        return main()
+    if a == "2":
+        print ("Chao")
     else:
-        print("por favor celeccione una opcion valida")
-        return Opciones
+        return darInicio()
+
+
     
 def agregarProductos  ():
-    print ("eeeeeeeeeeeeeeeee")
+    Producto = input("Digite el nombre del producto: ")
+    Precio = input("Digite el precio del producto: ")
+    Cantidad = input("digite las unidades que hay del producto: ")
+    Productos(Producto,Precio, Cantidad)
+    print ("¡El producto se a guardado con exito!")
+    return darInicio()
+
+
 def agregarACarrito():
-    print ("aaaaaaaaaaaaaa")
-
-
-Opciones =input(""" 
-1 si desea añadir un nuevo producto a la tienda
-2 si desea añadir un producto al carrito\n
-Por favor digite su opcion: """)
-     
-     
+    pass
 
 
 
 
+def verProductos ():
+    Producto = Productos.ListaProductos()
+    
+    return(Producto)
+
+def main ():
+    Pregunta= input(""" 
+    1 si desea añadir un nuevo producto a la tienda
+    2 si desea añadir un producto al carrito
+    3 si desea ver los productos de la tienda\n
+    Por favor digite su opcion: """)
+
+    if Pregunta == "1":
+        return agregarProductos()
+    if Pregunta =="2":
+        return agregarACarrito()
+    if Pregunta =="3":
+        return verProductos()
+    else:
+        print ("Por favor digite una opcion valida")
+    
+
+    
 
 
-
-darOpcion()
+main()
